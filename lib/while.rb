2f0a5d56e99bd3f310950380966b47888e832c3e
serena Lee <dearserenalee@gmail.com>
2019-11-09 05:11:19 +0000

    Automatically backed up by Learn

--- lib/while.rb
+++ lib/while.rb
@@ -8,4 +8,13 @@ def using_while
 end
 end
 
+def using_until
+  levitation_force = 6
+  #your code here
+  
+  until levitation_force == 10 
+  puts "Winggardium Leviosa"
+    levitation_force +=
+  end
+end
 
